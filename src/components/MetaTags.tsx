b554f9738211393c24a3b17d37c896ced19567f3
erpview <p.jaworski@erp-view.pl>
2025-01-27 17:01:00 +0100

    Fix viewport settings for companies page and MetaTags component

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -331,9 +331,17 @@ export const MetaTags: React.FC<MetaTagsProps> = ({ pageData, title, description
 
   return (
     <Helmet>
-      <title>{metaTitle}</title>
+      <title>{metaTitle || baseMetaTags.siteName}</title>
       <meta name="description" content={metaDescription} />
-      <meta name="application-name" content={baseMetaTags.siteName} />
+      
+      {/* Viewport and mobile settings */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-touch-fullscreen" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="HandheldFriendly" content="true" />
       
       {/* Open Graph */}
       <meta property="og:title" content={metaTitle} />
