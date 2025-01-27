a154a48b2b9c7502b1c801a89e90709123b66fc8
erpview <p.jaworski@erp-view.pl>
2025-01-27 17:05:24 +0100

    Update meta tags and SEO files

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -331,17 +331,9 @@ export const MetaTags: React.FC<MetaTagsProps> = ({ pageData, title, description
 
   return (
     <Helmet>
-      <title>{metaTitle || baseMetaTags.siteName}</title>
+      <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
-      
-      {/* Viewport and mobile settings */}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-touch-fullscreen" content="yes" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="HandheldFriendly" content="true" />
+      <meta name="application-name" content={baseMetaTags.siteName} />
       
       {/* Open Graph */}
       <meta property="og:title" content={metaTitle} />
