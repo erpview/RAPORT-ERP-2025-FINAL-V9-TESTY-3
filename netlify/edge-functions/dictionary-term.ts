a58addb7e9d023b2b0bcd54c51d93d3ef8480e6c
erpview <p.jaworski@erp-view.pl>
2025-02-01 15:58:57 +0100

    Update edge function with base href tag for proper bootstrapping

--- netlify/edge-functions/dictionary-term.ts
+++ netlify/edge-functions/dictionary-term.ts
@@ -29,6 +29,7 @@ export default async function handler(request: Request, context: Context) {
 <html lang="pl">
 <head>
   <meta charset="UTF-8">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <!-- Icons -->
